46781eeb96d945480f881f1db7504cd025507e6a
Gregor Maclaine <thegregormaclaine@gmail.com>
2022-07-15 21:43:01 +0100

    Added textbox.js

--- src/sketch.js
+++ src/sketch.js
@@ -1,7 +1,20 @@
+const text_to_show = `Hey guys, did you know that in terms of male human and female Pokémon breeding, Vaporeon is the most compatible Pokémon for humans? Not only are they in the field egg group, which is mostly comprised of mammals, Vaporeon are an average of 3”03’ tall and 63.9 pounds, this means they’re large enough to be able handle human dicks, and with their impressive Base Stats for HP and access to Acid Armor, you can be rough with one.`;
+const text_ui = new TextBox();
+
+function mouseClicked() {
+  text_ui.handle_click();
+}
+
+function preload() {
+  text_ui.preload();
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
+  text_ui.show_text('Me', text_to_show);
 }
 
 function draw() {
   background(220);
+  text_ui.show();
 }
---
+++ src/textbox.js
@@ -0,0 +1,54 @@
+class TextBox {
+  static NAME_RECT = [60, 337, 170, 35];
+  static TEXT_RECT = [88, 413, 610, 140];
+
+  constructor() {
+    this.assets = {
+      ui: null
+    };
+
+    this.name = null;
+    this.text = null;
+    this.text_index = 0;
+  }
+
+  preload() {
+    this.assets.ui = loadImage('assets/speech-ui.png');
+  }
+
+  show() {
+    image(this.assets.ui, 0, 0, width, height);
+
+    if (this.name) {
+      textSize(22);
+      textAlign(CENTER, CENTER);
+      text(this.name, ...TextBox.NAME_RECT);
+
+      textSize(18);
+      textAlign(LEFT, TOP);
+      text(this.text.slice(0, ++this.text_index), ...TextBox.TEXT_RECT);
+    }
+  }
+
+  show_text(name, text) {
+    this.name = name;
+    this.text = text;
+    this.text_index = 0;
+  }
+
+  _mouse_in_text_area() {
+    const [x, y, w, h] = TextBox.TEXT_RECT;
+    if (mouseX < x) return false;
+    if (mouseX > x + w) return false;
+    if (mouseY < y) return false;
+    if (mouseY > y + h) return false;
+    return true;
+  }
+
+  handle_click() {
+    // If click in the text rect, skip to end of text
+    if (this.name && this._mouse_in_text_area()) {
+      this.text_index = this.text.length;
+    }
+  }
+}
