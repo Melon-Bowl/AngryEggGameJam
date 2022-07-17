d32d6455557589f5bd09fe82d7f527bc9c63220f
Melon-Bowl <50581434+Melon-Bowl@users.noreply.github.com>
2022-07-17 14:34:57 +0100

    Added more background and update 1-2

--- src/sketch.js
+++ src/sketch.js
@@ -1,6 +1,6 @@
 const text_ui = new TextBox();
 
-const background_files = ['school.png'];
+const background_files = ['school.png', 'school_building.jpg', 'staff_room.jpg'];
 const backgrounds = {};
 
 const music = new Tracks();
